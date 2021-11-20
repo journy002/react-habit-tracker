0c58da4f756648077157b0ef66d55f5c6c94531a
journy002 <joruny002@naver.com>
2021-11-20 18:14:57 +0900

    update

--- src/component/addInput.jsx
+++ src/component/addInput.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 
 class AddInput extends Component {
+    formRef = React.createRef();
   inputRef = React.createRef();
 
   onSubmit = (event) => {
     event.preventDefault();
     const name = this.inputRef.current.value;
     name && this.props.onAdd(name) // 이름이 비어있지 않다면 onAdd라는 함수에 name을 전달해주는 코드
-    this.inputRef.current.value = '';
+    // this.inputRef.current.value = '';
+    this.formRef.current.reset(); // 초기화 시키는 방법은 위아래 두가지로 사용할 수 있음.
   };
 
   render() {
     return (
-      <form className="add-form" onSubmit={this.onSubmit}>
+      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
         <input
           ref={this.inputRef}
           type="text"
